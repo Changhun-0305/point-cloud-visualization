6076750d496f6031bea9c336937214c453d94ce8
Changhun-0305 <changhun0305@gmail.com>
2020-05-20 15:52:46 +0900

    pass points to template

--- cloud/points/views.py
+++ cloud/points/views.py
@@ -2,13 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 import numpy as np
+import json
+import array
 
 # Create your views here.
 def index(request):
-    points = np.fromfile("data/lidar/0001.bin", dtype=np.float32).reshape(-1, 4)
-    print(points)
+    frames = []
+    for i in range(3):
+        if i < 10:
+            path = "data/lidar/000{}.bin".format(i)
+        else:
+            path = "data/lidar/00{}.bin".format(i)
+        frames.append(np.fromfile(path, dtype=np.float32).reshape(-1, 4).tolist())
+
+        
     template = loader.get_template('points/index.html')
     context = {
-        
+        'frames': frames
     }
     return HttpResponse(template.render(context, request))
